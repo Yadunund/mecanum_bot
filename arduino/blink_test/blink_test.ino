c7ddd77b0fb2a68611c8be39b5887ab305207264
Yadunund Vijay <yadunund@openrobotics.org>
2020-09-20 23:48:51 +0800

    LED updates with serial data

--- arduino/blink_test/blink_test.ino
+++ arduino/blink_test/blink_test.ino
@@ -4,7 +4,6 @@ String data;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Hello: inside begin");
   pinMode(11, OUTPUT);
 
 }
@@ -25,16 +24,21 @@ void loop() {
 
   if (data.length() > 0)
   {
-    Serial.print("Received data of length ");
-    Serial.print(data.length());
-    Serial.println(" with content "+ data);
-    Serial.print("Integer value: ");
-    Serial.println(data.toInt());
+//    Serial.print("Received data of length ");
+//    Serial.print(data.length());
+//    Serial.println(" with content "+ data);
+//    Serial.print("Integer value: ");
+//    Serial.println(data.toInt());
 
     analogWrite(11, data.toInt());
-    delay(1000);
-    analogWrite(11, 0);
-
+//    delay(100);
   }
+
+//  else
+//  {
+//    analogWrite(11, 0);
+//  }
+
+
   
 }
