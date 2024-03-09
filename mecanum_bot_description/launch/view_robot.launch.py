e1a6145d9fd047060459b027022143624288b48b
Yadunund <yadunund@gmail.com>
2024-03-09 15:33:13 +0800

    Add office world
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- mecanum_bot_description/launch/view_robot.launch.py
+++ mecanum_bot_description/launch/view_robot.launch.py
@@ -16,28 +16,64 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
 
+
 def generate_launch_description():
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+                'robot_name',
+                default_value='mecanum_bot',
+                description='The name of the robot sdf model.'
+            )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+                'robot_sdf',
+                default_value=os.path.join(
+                    get_package_share_directory('mecanum_bot_description'),
+                    'mecanum_bot',
+                    'model.sdf'
+                ),
+                description='The name of the robot sdf model.'
+            )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+                'rviz',
+                default_value='true',
+                description='Open RViz.'
+            )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'world',
+            default_value='example_world.sdf',
+            description='Filename of the world in the world subdirectory'
+        )
+    )
+
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
+
+def launch_setup(context, *args, **kwargs):
+    robot_name = LaunchConfiguration('robot_name')
+    robot_sdf = LaunchConfiguration('robot_sdf')
+    rviz_launch_arg = LaunchConfiguration('rviz')
+    world = LaunchConfiguration('world')
 
     pkg_ros_gz_sim_demos = get_package_share_directory('mecanum_bot_description')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-
-    sdf_file = os.path.join(pkg_ros_gz_sim_demos, 'mecanum_bot', 'model.sdf')
     rviz_file = os.path.join(pkg_ros_gz_sim_demos, 'rviz', 'view_robot.rviz')
 
-    with open(sdf_file, 'r') as infp:
-        robot_desc = infp.read()
-
-    rviz_launch_arg = DeclareLaunchArgument(
-        'rviz', default_value='true',
-        description='Open RViz.'
-    )
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -46,13 +82,13 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_ros_gz_sim_demos,
             'worlds',
-            'example_world.sdf'
+            world
         ])}.items(),
     )
 
     # Bridge to forward tf and joint states to ros2
-    gz_topic = '/model/mecanum_bot'
-    joint_state_gz_topic = '/world/example_world' + gz_topic + '/joint_state'
+    gz_topic = '/model/' + robot_name.perform(context)
+    joint_state_gz_topic = '/world/' + world.perform(context).split('.')[0] + gz_topic + '/joint_state'
     link_pose_gz_topic = gz_topic + '/pose'
     bridge = Node(
         package='ros_gz_bridge',
@@ -88,6 +124,8 @@ def generate_launch_description():
     )
 
     # Get the parser plugin convert sdf to urdf using robot_description topic
+    with open(robot_sdf.perform(context), 'r') as infp:
+        robot_desc = infp.read()
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -104,16 +142,15 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         arguments=['-d', rviz_file],
-        condition=IfCondition(LaunchConfiguration('rviz')),
+        condition=IfCondition(rviz_launch_arg),
         parameters=[
             {'use_sim_time': True},
         ]
     )
 
-    return LaunchDescription([
-        rviz_launch_arg,
+    return [
         gazebo,
         bridge,
         robot_state_publisher,
         rviz
-    ])
+    ]
